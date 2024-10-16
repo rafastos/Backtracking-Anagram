84fc7513691769e3e2bf538e1dd9b80a27f67adb
Rafael S. Lima <rafaelkra2@gmail.com>
2024-10-15 22:29:08 -0300

    função geraCandidatos

--- anagrama.c
+++ anagrama.c
@@ -14,3 +14,22 @@ void processaSolucao(char *str)
 {
   printf("%s\n", str);
 }
+
+int geraCandidatos(char *str, int inicio, int fim, int *candidatos)
+{
+  int cont = 0;
+  int deveTrocar;
+  for (int i = fim; i >= inicio; i--)
+  {
+    deveTrocar = 1;
+    for (int j = inicio; j < i; j++)
+      if (str[j] == str[i])
+      {
+        deveTrocar = 0;
+        break;
+      }
+    if (deveTrocar)
+      candidatos[cont++] = i;
+  }
+  return cont;
+}
